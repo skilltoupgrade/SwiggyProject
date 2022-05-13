10e2b22f08d7458117d3a94c55a1801e9db5d669
Jyothi <Jyothi@harman.com>
2022-05-13 11:37:43 +0530

    developed logout.py

---
+++ logout.py
@@ -0,0 +1,5 @@
+if the user is logged in ==> clicks on logout ==> logout the user from DB/Session from the server
+
+if the user is logged in but session timed out ==> clicks on logout ==> throw the message "session timed out"
+
+if the user is not logged in ==> clicks on logout ==> redirect him to login page
