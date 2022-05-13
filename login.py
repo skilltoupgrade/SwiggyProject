df0096bbfa5df28b1fd106385671c114c5a8910c
Jyothi <Jyothi@harman.com>
2022-05-13 11:27:16 +0530

    fixed the production bug for login.py

--- login.py
+++ login.py
@@ -2,5 +2,5 @@ enter a valid UN
 enter a valid PWD
 
 upon clicking on login button, check if the UN & PWD are present in DB
-if Yes, success
-if No, error
+if Yes, let the user be logged in
+if No, let the user be prompted to retry
